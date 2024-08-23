0dedc593dc0dbc2086c8503ed1e9d83653e02785
luckyhyom <bs_khm@naver.com>
2024-08-23 23:59:47 +0900

    배열을 인자로 사용할 때 포인터를 사용해야 하는 이유와 한계
    사용 이유
    1. 배열의 다음 원소에 접근하기 위해서
    
    한계
    1. 정의된 매개 변수가 배열인지 아닌지 알 수 없다.
    2. 매개변수로 정의된 배열의 길이가 고정되어 있다.

--- 13-2.argurment-argument.c
+++ 13-2.argurment-argument.c
@@ -2,6 +2,8 @@
 
 int bank(int *money);
 int swap(int *a, int *b);
+int add_number(int *arr);
+int print_arr(int *arr);
 
 int main() {
     printf("이자 받기 \n");
@@ -17,6 +19,12 @@ int main() {
     swap(&my_money, &your_money);
     printf("[교환 후] my_money : %d, your money : %d \n", my_money, your_money);
 
+    printf("-----배열을 인자로------\n");
+
+    int arr[3] = {1,2,3};
+    print_arr(arr);
+    add_number(arr);
+    print_arr(arr);
     return 0;
 }
 
@@ -34,3 +42,27 @@ int swap(int *a, int *b) {
     *b = temp;
     return 0;
 };
+
+/*
+    1차원 배열이 매개변수일 때의 문제
+        1. 배열의 원소인지 사실 여부를 알 수 없다, 배열이 아닐 수도 있다!
+        2. 배열의 길이를 알아야한다. (길이가 고정된다.)
+*/
+int add_number(int *arr) {
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        arr[i]++;
+    }
+    return 0;
+}
+
+int print_arr(int *arr) { // 포인터(주소)를 받지 않으면 배열의 다음 원소를 알 수 없다.
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        printf("%d,", arr[i]);
+    }
+    printf("\n");
+    printf("----------------\n");
+}
