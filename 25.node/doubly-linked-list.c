1acc2c048b42df03612366871cf22193d6c7e68f
luckyhyom <bs_khm@naver.com>
2024-09-17 03:30:49 +0900

    양방향 연결 리스트

---
+++ 25.node/doubly-linked-list.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev_node;
+    struct Node *next_node;
+};
+
+struct Node* create_node(int data);
+struct Node* insert_node(struct Node* node, int data); // 이전 노드를 찾아야한다.
+void delete_node(struct Node* node);
+void print_node(struct Node* head);
+int count_node(struct Node* head);
+
+int main() {
+    struct Node *node1 = create_node(100);
+    struct Node *node2 = insert_node(node1, 200);
+    struct Node *node3 = insert_node(node2, 300);
+    struct Node *node4 = insert_node(node2, 400);
+    print_node(node1); // 100, 200, 400, 300
+    delete_node(node2);
+    print_node(node1); // 100, 400, 300
+
+    printf("total : %d \n", count_node(node1));
+}
+
+struct Node* create_node(int data){
+    struct Node *new_node;
+    new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->prev_node = NULL;
+    new_node->next_node = NULL;
+    return new_node;
+}
+
+struct Node* insert_node(struct Node* node, int data){
+    struct Node *next = node->next_node;
+
+    struct Node *new_node;
+    new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->prev_node = node;
+    new_node->next_node = next;
+
+    node->next_node = new_node;
+    return new_node;
+}
+
+void delete_node(struct Node* node){
+    node->prev_node->next_node = node->next_node;
+    node->next_node->prev_node = node->prev_node;
+    free(node);
+}
+
+void print_node(struct Node* from){
+    struct Node* next = from;
+    while (next)
+    {
+        printf("%d ", next->data);
+        next = next->next_node;
+    }
+    printf("\n");
+}
+
+int count_node(struct Node* head){
+    int count = 0;
+    struct Node* next = head;
+    while (next)
+    {
+        next = next->next_node;
+        count++;
+    }
+    return count;
+}
